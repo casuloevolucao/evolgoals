bd5fef31480d3bbf212ee65efa6bb37a999b7f9b
ismaelalvesbit <cearaismael1997@gmail.com>
2019-04-17 12:27:58 -0300

    atualizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeService } from '../services/home.service';
 import { Equipe } from '../models/equipe.model';
-import { LoginService } from '../services/login.service';
-import { Usuario } from '../models/usuario.model';
 
 @Component({
   selector: 'app-home',
@@ -15,26 +13,13 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private homeS:HomeService,
-    private loignS:LoginService,
   ) { }
 
   ngOnInit() {
     
   }
   submit(){
-    let usuario:Usuario = new Usuario()
-    usuario.email = "ismael@teste.com"
-    usuario.senha = "85926302"
-    usuario.nome = "ismael alves"
-
-    let usuario2:Usuario = new Usuario()
-    usuario2.email = "ismael@alves.com"
-    usuario2.senha = "85926302"
-    this.loignS.loginGoogle().then((rs)=>{
-      console.log(rs)
-    }).catch((e)=>{
-      console.log(e)
-    })
+    
   }
 
 }
